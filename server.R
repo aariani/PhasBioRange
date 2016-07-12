f2c87057a44d4e238570f69a1a83d271d5865681
aariani <aariani@ucdavis.edu>
2016-07-11 18:09:36 -0700

    Improved efficiency

--- server.R
+++ server.R
@@ -1,41 +1,40 @@
-## server.R
+## server
+
 library(vioplot)
 source('helpers.R')
-
-## Load data
-## This database will have a species col, a pool color (with 'W' or 'D')
 db=read.table('data/data.csv', sep='\t', header=T)
 
 shinyServer(function(input, output){
+## Choose pool data
 
-	output$violinplots=renderPlot({
-
-### Determine the dataset to use
-		pool=switch(input$data,
-			'All'='All',
+	pool=reactive({
+		switch(input$data,
+			'All'='All', 
 			'Wild'='Wild',
 			'Domesticated'='Cultivated'
 			)
+		})
 
-### Get the dataset based on the input$data filed
-		finalData=reactive({
-			if (pool=='All') return(db)
-			processDB(db, pool) ## subsample fo the database
-			})
-		
-##### chose the color
+### Chose final data
+	finalData=reactive({
+		if (pool() =='All') return(db)
+		processDB(db, pool())
+		})
+### Choose vars
+	vars=reactive({
+		switch(input$var, ### modify the name in the table according to your data
+			'Annual Precipitation'='prec',
+			'Annual Mean Temperature'='temp',
+			'Altitude'='alt'
+			)
+		})
+
+	output$violinplots=renderPlot({
 		color=switch(input$var,
 			'Annual Precipitation'='blue',
 			'Annual Mean Temperature'='red',
 			'Altitude'='green4'
-			)
-
-### Determine the variable to plot
-                vars=switch(input$var, ### modify the name in the table according to your data
-                        'Annual Precipitation'='prec',
-                        'Annual Mean Temperature'='temp',
-                        'Altitude'='alt'
-                        )
+			)			
 
 		yleg=switch(input$var,
 			'Annual Precipitation'='mm', 
@@ -43,8 +42,7 @@ shinyServer(function(input, output){
 			'Altitude'='meters'
 			)
 
-		plotData(finalData(), vars, color, input$var, yleg)
-                
-		})
-	})
+		plotData(finalData(), vars(), color, input$var, yleg)
+		})	
 
+	})
